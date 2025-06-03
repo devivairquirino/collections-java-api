6989d858b61ced6a2fc976f0d45dd329d9860b00
DEV Ivair Quirino <dev.ivairquirino@gmail.com>
2025-06-03 19:40:54 -0300

    Implement ConjuntoConvidados and Convidado classes with methods for managing guest invitations

---
+++ src/set/operacoes_basicas/ConjuntoConvidados.java
@@ -0,0 +1,49 @@
+package set.operacoes_basicas;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConjuntoConvidados {
+    private Set<Convidado> convidadoSet;
+
+    public ConjuntoConvidados(Set<Convidado> convidadoSet) {
+        this.convidadoSet = new HashSet<>();
+    }
+
+    public void adicionarConvidado(String nome, int codigoConvite) {
+        convidadoSet.add(new Convidado(nome, codigoConvite));
+    }
+    public int contarConvidados(){
+        return convidadoSet.size();
+    }
+    public void exibirConvidados() {
+        System.out.println(convidadoSet);
+    }
+    public void removerConvidadoPorCodigoConvite(int codigoConvite){
+        Convidado convidadoRemover= null;
+
+        for(Convidado c: convidadoSet){
+            if(c.getCodigoConvite()==codigoConvite){
+                convidadoRemover=c;
+                break;
+            }
+        }
+        convidadoSet.remove(convidadoRemover);
+    }
+
+    public static void main(String[] args) {
+
+        ConjuntoConvidados conjunto= new ConjuntoConvidados(null);
+
+        conjunto.adicionarConvidado("Ana", 101);
+        conjunto.adicionarConvidado("Bruno", 102);
+        conjunto.adicionarConvidado("Carla", 103);
+        conjunto.adicionarConvidado("Daniel", 104);
+        conjunto.adicionarConvidado("Eduarda", 105);
+
+        conjunto.removerConvidadoPorCodigoConvite(102);
+        conjunto.exibirConvidados();
+
+        System.out.println(conjunto.contarConvidados());
+    }
+}
---
+++ src/set/operacoes_basicas/Convidado.java
@@ -0,0 +1,26 @@
+package set.operacoes_basicas;
+
+public class Convidado {
+    private String nome;
+    private int codigoConvite;
+    
+    
+    public Convidado(String nome, int codigoConvite) {
+        this.nome = nome;
+        this.codigoConvite = codigoConvite;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public int getCodigoConvite() {
+        return codigoConvite;
+    }
+
+    @Override
+    public String toString() {
+        return "\nConvidado [nome=" + nome + ", codigoConvite=" + codigoConvite  + "\n";
+    }
+    
+
+}
