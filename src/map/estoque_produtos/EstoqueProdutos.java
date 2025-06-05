92c54d191138f4c741eb2b2d83f06aef1e826286
DEV Ivair Quirino <dev.ivairquirino@gmail.com>
2025-06-05 16:15:32 -0300

    Enhance EstoqueProdutos class with additional methods for inventory management, including calculations for total stock value, and retrieval of the most expensive, cheapest, and highest quantity value products.

--- src/map/estoque_produtos/EstoqueProdutos.java
+++ src/map/estoque_produtos/EstoqueProdutos.java
@@ -5,29 +5,98 @@ import java.util.Map;
 
 public class EstoqueProdutos {
     private Map<Long, Produto> estoqueProdutosMap;
-    
-    public EstoqueProdutos(){
-        this.estoqueProdutosMap= new HashMap<>();
+
+    public EstoqueProdutos() {
+        this.estoqueProdutosMap = new HashMap<>();
 
     }
-    public void adicionarProduto(long cod, String nome, int quantidade, double preco){
+
+    public void adicionarProduto(long cod, String nome, int quantidade, double preco) {
         estoqueProdutosMap.put(cod, new Produto(nome, quantidade, preco));
     }
 
-    public void exibirEstoque(){
+    public void exibirEstoque() {
         System.out.println(estoqueProdutosMap);
     }
 
+    public Double calcularValorTotalEstoque() {
+        Double result = 0.0;
+
+        for (Map.Entry<Long, Produto> produto : estoqueProdutosMap.entrySet()) {
+
+            int quantidade = produto.getValue().getQuantidade();
+            Double preco = produto.getValue().getPreco();
+
+            result += quantidade * preco;
+        }
+        return result;
+    }
+
+    public Produto obterProdutoMaisCaro() {
+        Produto produtoMaisCaro = null;
+        Double maisCaro = Double.MIN_VALUE;
+
+        for (Map.Entry<Long, Produto> produto : estoqueProdutosMap.entrySet()) {
+            Double preco = produto.getValue().getPreco();
+            if (preco > maisCaro) {
+                maisCaro = preco;
+                produtoMaisCaro = produto.getValue();
+            }
+        }
+
+        return produtoMaisCaro;
+    }
+
+    public Produto obterProdutoMaisBarato() {
+        Produto produtoMaisBarato = null;
+        Double maisBarato = Double.MAX_VALUE;
+
+        for (Produto p : estoqueProdutosMap.values()) {
+            Double preco = p.getPreco();
+            if (preco < maisBarato) {
+                maisBarato = preco;
+                produtoMaisBarato = p;
+            }
+        }
+
+        return produtoMaisBarato;
+
+    }
+
+    // Retorna o produto que está em maior quantidade no estoque, considerando o
+    // valor total de cada produto (quantidade * preço).
+    public Produto obterProdutoMaiorQuantidadeValorTotalNoEstoque() {
+        Produto ProdutoMaiorQuantidadeValorTotal = null;
+        Double maiorQuantidadeValorTotal = Double.MIN_VALUE;
+
+        for (Produto p : estoqueProdutosMap.values()) {
+            Double precoTotal = p.getPreco()*p.getQuantidade();
+
+            if (precoTotal > maiorQuantidadeValorTotal) {
+                maiorQuantidadeValorTotal = precoTotal;
+                ProdutoMaiorQuantidadeValorTotal = p;
+            }
+        }
+
+        return ProdutoMaiorQuantidadeValorTotal;
+
+    }
+
     public static void main(String[] args) {
-        EstoqueProdutos novoEstoque= new EstoqueProdutos();
-        
+        EstoqueProdutos novoEstoque = new EstoqueProdutos();
+
         novoEstoque.adicionarProduto(1L, "Notebook", 10, 3500.00);
-        novoEstoque.adicionarProduto(2L, "Mouse", 50, 80.00);
+        novoEstoque.adicionarProduto(2L, "Mouse", 500, 80.00);
         novoEstoque.adicionarProduto(3L, "Teclado", 30, 120.00);
         novoEstoque.adicionarProduto(4L, "Monitor", 15, 900.00);
         novoEstoque.adicionarProduto(5L, "Impressora", 8, 650.00);
 
-        novoEstoque.exibirEstoque();
+        // novoEstoque.calcularValorTotalEstoque();
+        System.out.println(novoEstoque.obterProdutoMaisCaro());
+        System.out.println(novoEstoque.obterProdutoMaisBarato()); 
+        System.out.println(novoEstoque.obterProdutoMaiorQuantidadeValorTotalNoEstoque()); 
+
+        // novoEstoque.exibirEstoque();
 
     }
 }
